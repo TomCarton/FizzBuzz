a1418b13c4617608960597dd6041cfec479be40f
tom <thomas.carton@23h23.com>
2017-01-25 23:50:45 +0100

    Added some more versions buzzing my mind

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -3,8 +3,8 @@
 
 #include <stdio.h>
 
-
-int main(int argc, const char *argv[])
+// version 1: using tests and a temporary flag
+void version_1()
 {
     for (unsigned int i = 1; i <= 100; ++i)
     {
@@ -31,6 +31,42 @@ int main(int argc, const char *argv[])
 
         printf("\n");
     }
+}
 
+// version 2: with only one branching
+void version_2()
+{
+    char fizz[] = "Fizz\0---\0---\0";
+    char buzz[] = "Buzz\0---\0---\0---\0---\0";
+    char r[] = "??zz--zz";
+    
+    for (unsigned int i = 1; i <= 100; ++i)
+    {
+        sprintf(r, "%d", i);
+        if (!(i % 3) || !(i % 5))
+            sprintf(r, "%s%s", &fizz[4 * (i % 3)], &buzz[4 * (i % 5)]);
+        printf("%s\n", r);
+    }
+}
+
+// version 3: first attempt with no branching
+void version_3()
+{
+    char zz[] = "FizzBuzz\0Fizz\0zzzzFizz\0zzzzFizz\0zzzzFizz\0zzzzBuzz\0zzzz\0zzzzzzzz\0zzzzzzzz"
+                "\0zzzzzzzz\0zzzzzzzzBuzz\0zzzz\0zzzzzzzz\0zzzzzzzz\0zzzzzzzz\0zzzzzzzz";
+                
+    for (unsigned int i = 1; i <= 100; ++i)
+    {        
+        printf("%d %s\n", i, &zz[((i % 3) * 5 + i % 5) * 9]);
+    }
+}
+
+
+int main(int argc, const char *argv[])
+{
+    printf("\n\nVersion 1\n---------\n\n"); version_1();
+    printf("\n\nVersion 2\n---------\n\n"); version_2();
+    printf("\n\nVersion 3\n---------\n\n"); version_3();
+        
     return 0;
 }
